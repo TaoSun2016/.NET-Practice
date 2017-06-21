c28e81275e5d1b324c90e7a69f24617e2b7b8ce1
TaoSun2016 <kevinsun9516@gmail.com>
2017-06-21 20:27:21 +1200

    init project

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "Hello from Store.Index()";
+        }
+
+        // GET: Browse
+       /* public string Browse()
+        {
+            return "Hello from Store.Browse()";
+        }
+        */
+
+        // GET: Browse
+        public string Browse(string genre)
+        {
+            string message = HttpUtility.HtmlEncode("Store/browse Genre =" + genre);
+            return message;
+        }
+
+        // GET: Details
+        public string Details(int id)
+        {
+            string message = "Store:Details ID=" + id;
+            return message;
+        }
+    }
+}
